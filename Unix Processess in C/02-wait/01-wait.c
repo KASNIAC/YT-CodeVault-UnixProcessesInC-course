ae3ccbc706d673e089dfc16d508532fe8fc626ff
kasniac <oagg_2000@hotmail.com>
2023-11-11 17:56:49 -0600

    Video 02

---
+++ Unix Processess in C/02-wait/01-wait.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include <sys/wait.h>
+
+int main(int argc, char* argv[]){
+    int id = fork();
+    int n;
+
+    if(id == 0){
+        n = 1;
+    } else{
+        wait(NULL);     // We jsut have to call it in the main process in order to wait the child process finish its work
+        n = 6;
+    }
+    
+    for(int i = n; i<n + 5; ++i){
+        printf("%d ", i);
+        fflush(stdout);     // Flushing the standar output (due the buffer it might wait to print all togheter, that's why we use fflush)
+    }
+
+    if(id != 0){
+        printf("\n");
+    }
+    
+    return 0;
+}
