c29e5f4c1defa31e53c8efb11500bac414a537de
kasniac <oagg_2000@hotmail.com>
2023-11-19 23:09:39 -0600

    Video 06

---
+++ Unix Processess in C/06-pipes.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+// Pipes let us communicate processes. 
+// A pipe is a in memory file that only has sort of a buffer that is saved in memory and you can write and read from it
+int main(int argc, char* argv[]){
+    int fd[2]; // file descriptors for the pipe (a file descriptor is basically a key to access to a file)
+    // fd[0] - read
+    // fd[1] - write
+
+    // pipe(fd); It's going to save the file descriptors that it opens when we create this pipe
+    if(pipe(fd) == -1){
+        printf("An error ocurred with opening the pipe\n");
+        return 1;
+    }
+
+    int id = fork();
+    if(id == -1){
+        printf("An error ocurred with fork\n");
+        return 4;
+    }
+    if(id == 0){
+        close(fd[0]); // I'm not going to read nothing from this file
+        // It used to be the standar, while a process write, the other read and vice versa
+
+        int x;
+        printf("Input a number: ");
+        scanf("%d", &x);
+        if(write(fd[1], &x, sizeof(int)) == -1){
+            printf("An error ocurred with writing to the pipe\n");
+            return 2;
+        }
+
+        // tells anybody that might be listening on the other end that this file has been closed and you don't have anything to read from it 
+        // (there's no longer any file descriptor that can write to it open)
+        close(fd[1]);   
+    } else{
+        close(fd[1]); // I'm not going to write nothing on this file
+
+        int y;
+        if(read(fd[0], &y, sizeof(int)) == -1){
+            printf("An error ocurred with reading from the pipe\n");
+            return 3;
+        }
+
+        close(fd[0]);
+
+        printf("Got from child process: %d\n", y);
+    }
+
+
+    return 0;
+}
---
+++ delete.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Utiliza git clean para eliminar archivos no rastreados según .gitignore
+git clean -X -f
