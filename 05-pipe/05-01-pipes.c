537660ad812c4dee8986b9112a5048c2c72b2959
KASNIAC <kasniac10@gmail.com>
2024-03-18 20:12:53 -0400

    pipe

--- 05-pipe/05-01-pipes.c
+++ 05-pipe/05-01-pipes.c
@@ -1,17 +1,19 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-#include<unistd.h>
+#include<unistd.h> // write, read, close
+#include<sys/wait.h>
+#include<errno.h>
 
 // Pipes let us communicate processes. 
-// A pipe is a in memory file that only has sort of a buffer that is saved in memory and you can write and read from it
+// A pipe is an in memory file that only has sort of a buffer that is saved in memory and you can write and read from it
 int main(int argc, char* argv[]){
     int fd[2]; // file descriptors for the pipe (a file descriptor is basically a key to access to a file)
     // fd[0] - read
     // fd[1] - write
 
     // pipe(fd); It's going to save the file descriptors that it opens when we create this pipe
-    if(pipe(fd) == -1){
+    if (pipe(fd) == -1) {
         printf("An error ocurred with opening the pipe\n");
         return 1;
     }
@@ -20,8 +22,7 @@ int main(int argc, char* argv[]){
     if(id == -1){
         printf("An error ocurred with fork\n");
         return 4;
-    }
-    if(id == 0){
+    } else if (id == 0) { // I'm the children process
         close(fd[0]); // I'm not going to read nothing from this file
         // It used to be the standar, while a process write, the other read and vice versa
 
@@ -36,7 +37,7 @@ int main(int argc, char* argv[]){
         // tells anybody that might be listening on the other end that this file has been closed and you don't have anything to read from it 
         // (there's no longer any file descriptor that can write to it open)
         close(fd[1]);   
-    } else{
+    } else { // I'm the parent process
         close(fd[1]); // I'm not going to write nothing on this file
 
         int y;
@@ -50,6 +51,6 @@ int main(int argc, char* argv[]){
         printf("Got from child process: %d\n", y);
     }
 
-
+    // Since read is a blocking call, wait(NULL) won't be necessary. The parent will wait until the children writes into the pipe.
     return 0;
 }
